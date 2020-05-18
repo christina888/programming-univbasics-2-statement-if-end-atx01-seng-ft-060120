80a1af32de600cc07a7d27a890efe0b85835f818
Christina Chianelli <cechianelli@gmail.com>
2020-05-18 23:19:20 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,12 +1,4 @@
 
-current_second = Time.now
-current_second = current_second.to_i
-if current_second % 2 == 0 
-  puts "Even!"
-else
-  puts "Odd!"
-end 
-
 if Time.now.to_i % 2 == 0 
   puts "Even!"
 else 
