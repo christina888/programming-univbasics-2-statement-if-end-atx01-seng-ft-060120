445e3a80f52283cffe911a9544b598165c267769
Christina Chianelli <cechianelli@gmail.com>
2020-05-18 22:13:57 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -7,3 +7,11 @@ if run_code_inside
 end
 puts "Code after if...end"
 
+chance_of_rain = 0.2 
+puts "Let's go outside!"
+if chance_of_rain > 0.5
+  puts "pack an umbrella!"
+else 
+  puts "Enjoy the fine day!"
+end
+puts "Oh, and always wear sunscreen!"
