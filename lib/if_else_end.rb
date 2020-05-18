3eaafe544e782519c5415f9a583b45b1af549025
Christina Chianelli <cechianelli@gmail.com>
2020-05-18 22:16:10 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -8,10 +8,17 @@ end
 puts "Code after if...end"
 
 chance_of_rain = 1.0 
-puts "Let's go outside!"
-if chance_of_rain > 0.5
+puts "Let's go outside!" 
+    #starting statement...stays constant
+if chance_of_rain > 0.5 #conditional statement...
   puts "pack an umbrella!"
 else 
   puts "Enjoy the fine day!"
 end
-puts "Oh, and always wear sunscreen!"
+puts "Oh, and always wear sunscreen!" 
+    #end statement...also stays constant
+
+#So our formula looks like this:
+#"Let's go outside!"
+#A: Pack an umbrella! B) Enjoy the fine day!
+#"Oh, and always wear sunscreen!""
